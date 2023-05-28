0bf483363e9d34f02c907024fcb41406f82817e8
Prafful Indirala <prafful.log@gmail.com>
2023-05-29 00:12:48 +0100

    files

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function testpage() {
+  return <div>Test Page</div>;
+}
---
+++ src/atoms/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ButtonProps = {
+  children: "";
+  onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
