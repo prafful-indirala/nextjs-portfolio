f7b07ed3de1732229d8eb1360be9f790520d4be6
Prafful Indirala <prafful.log@gmail.com>
2023-05-29 21:26:41 +0100

    code fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Main from "../components/Main";
-import Header from "../elements/Header";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Main />
       <Main />
     </div>
   );
