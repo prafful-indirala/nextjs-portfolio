fe2c63431b91bd288df3b113d56473becdaba8db
Prafful Indirala <prafful.log@gmail.com>
2023-05-29 20:31:22 +0100

    SOCIAL_MEDIA_URLS

---
+++ src/constants/socialMediaUrls.ts
@@ -0,0 +1,6 @@
+export const SOCIAL_MEDIA_URLS = {
+    instagram: 'https://www.instagram.com/_prafful_indirala_/',
+    twitter: 'https://twitter.com/PraffulIndirala',
+    github: 'https://github.com/prafful-indirala',
+    linkedin: 'https://www.linkedin.com/in/praffulindirala/',
+  };
